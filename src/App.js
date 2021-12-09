6f4bcc5014f72fe1573cbf7799ff0167524e2661
mabdullahi <mabdullahi@aptly.de>
2021-12-09 13:48:25 +0100

    after delete

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ function App() {
   return (
     <div>
       Hello {value} Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Animi, laudantium corrupti optio repellat sed cupiditate autem expedita
-      fuga perspiciatis veritatis in hic asperiores! Voluptates eaque commodi
-      natus officiis repellat ducimus.
+      Animi, laudantium corrupti optio repellat sed cupiditate autem expedita f
     </div>
   );
 }
