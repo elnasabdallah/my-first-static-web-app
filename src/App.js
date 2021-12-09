5652a575427eeb5e9ba80ee4319b15800cc705cf
mabdullahi <mabdullahi@aptly.de>
2021-12-09 13:37:34 +0100

    lorem

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function App() {
   const value = "World";
-  return <div>Hello {value} of Nas</div>;
+  return (
+    <div>
+      Hello {value} Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Animi, laudantium corrupti optio repellat sed cupiditate autem expedita
+      fuga perspiciatis veritatis in hic asperiores! Voluptates eaque commodi
+      natus officiis repellat ducimus.
+    </div>
+  );
 }
 
 export default App;
